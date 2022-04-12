3b748fcf440f1890b0931709563fde2ef885b87b
Andre Fagundes <andreflik@gmail.com>
2022-04-12 14:56:32 -0300

    sistema de ponto eletronico

---
+++ src/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <span>Desenvolvido por</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>Fagundes Info<span class="text-prymary"></span></span>
+</footer>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
